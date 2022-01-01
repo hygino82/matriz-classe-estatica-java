af448dda7e93eebbe7b50d74dc88dfd042af8d59
Adroaldo <hygino@msn.com>
2022-01-01 09:05:48 -0300

    Implementado o escalonamento e o backup da matriz

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,13 +9,26 @@ public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 
-		double[][] m = { { 1, 2, 3 }, { 4, 5, 6 } };
+		double[][] m = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 2 } };
 
 		Matriz.escrevaMatriz(m);
-		
-		double[] n= {1,2,3,4};
+		Matriz.escalona(m);
+		linha();
+		double[][] matrizEscalonada = Matriz.escalona(m);
+		System.out.println("Matriz escalonada");
+		Matriz.escrevaMatriz(matrizEscalonada);
+		linha();
+		System.out.println("Matriz original");
+		Matriz.escrevaMatriz(m);
+
+		linha();
+		double[] n = { 1, 2, 3, 4 };
 		Matriz.escrevaMatriz(n);
 
+	}
+
+	public static void linha() {
+		System.out.println("----------------------------------------------\n");
 	}
 
 }
--- src/model/Matriz.java
+++ src/model/Matriz.java
@@ -7,7 +7,7 @@ public class Matriz {
 
 		for (int i = 0; i < linhas; i++) {
 			for (int j = 0; j < colunas; j++) {
-				System.out.printf("%.5f\t", m[i][j]);
+				System.out.printf("%.5f  ", m[i][j]);
 			}
 			System.out.println();
 		}
@@ -17,9 +17,67 @@ public class Matriz {
 		int colunas = m.length;
 
 		for (int j = 0; j < colunas; j++) {
-			System.out.printf("%.5f\t", m[j]);
+			System.out.printf("%.5f  ", m[j]);
 		}
 		System.out.println();
 	}
+
+	public static double[][] escalona(double[][] m) {
+
+		double[][] t = matrizBackup(m);// fazer cópia da matriz para não modificar a original
+		int n = t.length;
+		int x, y, p;
+		double cte;// constante obtida usando o pivô na linha
+
+		for (p = 0; p < n; p++) {
+			if (t[p][p] == 0)
+				for (x = p + 1; x < n; x++)
+					if (t[x][p] != 0)
+						for (y = 0; y < n; y++)
+							t[p][y] = t[p][y] + t[x][y];
+			if (t[p][p] != 0) {
+				for (x = p + 1; x < n; x++) {
+					cte = t[x][p] / t[p][p];
+					for (y = 0; y < n; y++)
+						t[x][y] = t[x][y] - cte * t[p][y];
+				}
+			}
+		}
+		return t;
+	}
+
+	public static double[][] matrizBackup(double[][] m) {
+
+		double[][] matriz = new double[m.length][m[0].length];
+		for (int x = 0; x < m.length; x++) {
+			for (int y = 0; y < m[0].length; y++) {
+				matriz[x][y] = m[x][y];
+			}
+		}
+		return matriz;
+	}
+
+	public static double determinante(double[][] t) {
+
+		int n = t.length;
+		double det = 1; // elemento neutro em um produto
+		for (int z = 0; z < n; z++) {
+			det = det * t[z][z];
+		}
+		return det;
+	}
+
+//	public static void escrevaMatriz(Double[][] m) {
+//		int linhas = m.length;
+//		int colunas = m[0].length;
+//
+//		for (int i = 0; i < linhas; i++) {
+//			for (int j = 0; j < colunas; j++) {
+//				System.out.printf("%.5f  ", m[i][j]);
+//			}
+//			System.out.println();
+//		}
+//		
+//	}
 
 }
